f777700a2b24a809da6e5b5d66cd444671cc90ad
depthso <86912923+depthso@users.noreply.github.com>
2023-10-21 08:50:26 +0100

    Update string.js

--- Monaco/vs/basic-languages/lua/autocompletes/libraries/string.js
+++ Monaco/vs/basic-languages/lua/autocompletes/libraries/string.js
@@ -17,50 +17,50 @@ define(["require", "exports"], function (require, exports) {
 			},
 
 			'find()': {
-				insertText: 'defer(${1:string}, ${2:pattern}, ${3:init})',
+				insertText: 'find(${1:string}, ${2:pattern}, ${3:init})',
 				documentation: {
 					value: ""
 				}
 			},
 
 			'split()': {
-				insertText: 'delay(${1:string}, ${2:seperator})',
+				insertText: 'split(${1:string}, ${2:seperator})',
 				documentation: {
 					value: ""
 				}
 			},
 
-      'len()': {
+      			'len()': {
 				insertText: 'len(${1:string})',
 				documentation: {
 					value: "Returns the length of the string"
 				}
 			},
-      'rep()': {
+      			'rep()': {
 				insertText: 'rep(${1:string}, ${2:number})',
 				documentation: {
 					value: ""
 				}
 			},
-      'gsub()': {
+      			'gsub()': {
 				insertText: 'gsub(${1:string}, ${2:pattern}, ${3:replacement}, ${4:replacements})',
 				documentation: {
 					value: ""
 				}
 			},
-      'upper()': {
+      			'upper()': {
 				insertText: 'upper(${1:string})',
 				documentation: {
 					value: ""
 				}
 			},
-      'lower()': {
+     			 'lower()': {
 				insertText: 'lower(${1:string})',
 				documentation: {
 					value: ""
 				}
 			},
-      'reverse()': {
+      			'reverse()': {
 				insertText: 'reverse(${1:string})',
 				documentation: {
 					value: ""
