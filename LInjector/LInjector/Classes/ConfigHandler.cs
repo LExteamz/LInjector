c647a46bfe7504a3a168d3c2e69e3eea92e19321
NeverExcel <147359654+NeverExcel@users.noreply.github.com>
2023-12-29 01:55:01 -0600

    LInjecter Update

--- LInjector/LInjector/Classes/ConfigHandler.cs
+++ LInjector/LInjector/Classes/ConfigHandler.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using System.Windows.Forms;
 
 namespace LInjector.Classes
 {
--- Redistributables/Lua Scripts/Init.lua
+++ Redistributables/Lua Scripts/Init.lua
@@ -10,7 +10,7 @@
 
 local EXPLOIT_NAME = "LInjector"
 local GITHUB_NAME = "LExteamz"
-local EXPLOIT_VERSION = "11.11.2023"
+local EXPLOIT_VERSION = "09.12.2023"
 
 --[[local genv = getgenv()
 if genv[EXPLOIT_NAME] then
